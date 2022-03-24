ac0c992ebce36140eb592fb2891e326fd86c8014
Michał Gawryś <michal.gawrys@g2a.com>
2022-03-24 15:19:53 +0100

    another fix

--- proxmoxdriver.go
+++ proxmoxdriver.go
@@ -821,7 +821,7 @@ func (d *Driver) Create() error {
 			return err
 		}
 
-		err = d.driver.WaitForTaskToComplete(node, taskid)
+		err = d.driver.WaitForTaskToComplete(clone_node, taskid)
 		if err != nil {
 			return err
 		}
